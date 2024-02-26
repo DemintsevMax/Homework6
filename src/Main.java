ab2af593a1de342d47811c8bd0f2848d3fba6b84
Maksim <raymax.spb@gmail.com>
2024-02-26 18:23:53 +0300

    Домашнее задание 6

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
-        for (int i = 10; i <= 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println(i);
-
-
         }
+        System.out.println("Task 2");
+        for ()
     }
 }
