c6b03739825b4920ac9011455a165203c5d1dff4
Maksim <raymax.spb@gmail.com>
2024-02-26 19:32:16 +0300

    Домашнее задание 6

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,40 @@ public class Main {
             System.out.println(i);
         }
         System.out.println("Task 3");
-        for (int i = 0; i <= 17; i= i+2 ) {
+        for (int i = 0; i <= 17; i = i + 2) {
             System.out.println(i);
+        }
+        System.out.println("Task 4");
+        for (int i = -10; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Task 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i);
+        }
+        System.out.println("Task 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println("Task 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        System.out.println("Task 8");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + salary;
+
+            System.out.println(i + " Месяц сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Task 9");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + total / 100;
+            total = total + salary;
+            System.out.println(i + " Месяц сумма накоплений равна " + total + " рублей");
         }
     }
 }
